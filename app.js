c887e8dc2f64e9c68e70ec54b7b99745636e5027
aapetrova <aneliaapetrova@gmail.com>
2020-10-04 12:21:27 +0200

    add header

---
+++ app.js
@@ -0,0 +1,3 @@
+particlesJS.load('particles-js', 'https://raw.githubusercontent.com/aapetrova/particlesjs-config/master/particlesjs-config.json?token=AEV324M7BAM7PNHZ6HXF3WS7PV5UM', function() {
+  console.log('callback - particles.js config loaded');
+});
