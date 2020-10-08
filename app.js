bea7e0c0f4053a920b797556e0c6ae157cefe98b
Vivian Du <vivian_du@berkeley.edu>
2020-10-07 20:00:40 -0700

    fixed header

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load('particles-js', 'https://raw.githubusercontent.com/LuMesserschmidt/CoronaNet_V2/master/assets/particlesjs-config.json', function() {
   console.log('callback - particles.js config loaded');
 });
